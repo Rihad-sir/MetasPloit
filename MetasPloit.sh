f243887d8a1bb9943287fb0f6c9a03a8a15544e6
_wh04m!..⟳⚠️ <88466421+Rihad-sir@users.noreply.github.com>
2021-08-14 05:17:52 +0530

    Update MetasPloit.sh

--- MetasPloit.sh
+++ MetasPloit.sh
@@ -1,8 +1,21 @@
-pkg install figlet
+pkg install figlet -y
 clear
 figlet Msploit
-echo  installing postgresql...
-pkg install postgresql.
+echo installing postgresql...
+echo .                  .
+echo###############installing...
+echo.
+pkg install postgresql -y
+echo postgresql installed✅
+echo installing openssh wget curl git...
+echo.                       .
+echo###############installing...
 
+pkg install openssh wget curl git -y
+echo #####installd✅##########
 
+clear.
+figlet Msploit.
+echo ##########scripted by RIHAD NESSES CYBER SECURITY ™######
+echo *.                  .*
 wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh
