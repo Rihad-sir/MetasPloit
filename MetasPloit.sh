ae356a091245d36e1949c1c4ee8b5713ac160bd9
_wh04m!..⟳⚠️ <88466421+Rihad-sir@users.noreply.github.com>
2021-08-14 04:31:15 +0530

    Update MetasPloit.sh

--- MetasPloit.sh
+++ MetasPloit.sh
@@ -1,1 +1,8 @@
+pkg install figlet
+clear
+figlet Msploit
+echo  installing postgresql...
+pkg install postgresql.
+
+
 wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh
